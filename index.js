eaa2e4613501665e5d0b3e803609b2990be79c11
Mayeul <leuyamgod@gmail.com>
2024-05-30 17:57:57 +0200

    IOT

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
+import ip from "ip";
+import router from "./routes/start";
+
+
 const express = require("express");
 const cors = require("cors");
-const ip = require("ip")
 const app = express();
 const port = 3000;
 const ipAddr = ip.address();
 
-let lastHouseVisited;
+let lastHouseVisited = "Gryffondor"
+
 
 app.use(cors());
-//app.use("/", require("./routes/start"));
+
 app.use(express.json());
-app.use(express.static("public"));
+app.use(router)
 
-
-
-app.get("/tutu", (req, res) => {
-  return res.json({message : lastHouseVisited})
+app.get("/", (req, res) => {
+    res.json({message : lastHouseVisited})
 })
 
-app.post("/tutu", (req,res) => {
-  lastHouseVisited=req.body.lastHouseVisited;
-  return res.json({message : lastHouseVisited})
-  
+app.post("/", (req,res) => {
+    lastHouseVisited=req.body.house;
+    res.json({message:lastHouseVisited})
+    
 })
 
-console.log(lastHouseVisited)
 
 const initializeApp = () => {
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
-    console.log("Server run :" + ipAddr +":3000");
+    console.log("Server run :" + ipAddr)
   });
 };
 
